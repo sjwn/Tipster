4abcf836a2c8d6c4b19a90411972ce8307be53ec
Samantha Wong <Sammiu@pivotal-guest-536.sf.pivotallabs.com>
2015-09-26 10:26:04 -0700

    Initial Commit

---
+++ Tipster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tipster
+//
+//  Created by Samantha Wong on 2015-09-26.
+//  Copyright © 2015 Samantha Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
